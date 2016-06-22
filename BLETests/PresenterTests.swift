f2020a19549724614835508cd35c0e586add81c4
Dan Wallace <daniel.wallace.nz@gmail.com>
2016-06-22 16:20:57 +1200

    add onCreate test

--- BLETests/PresenterTests.swift
+++ BLETests/PresenterTests.swift
@@ -12,26 +12,48 @@ import XCTest
 class PresenterTests: XCTestCase {
     
     class MockSensorTag:SensorTag {
+        
         var startWasCalled = false
         var stopWasCalled = false
+        
         func start() {
             startWasCalled = true
         }
+        
         func stop() {
             stopWasCalled = true
         }
     }
     
-    
+    class View:UIInterface {
+        
+        var enableStartButtonWasCalled = false
+        var enableStartButtonArgument:Bool = false
+        var enableStopButtonWasCalled = false
+        var enableStopButtonArgument:Bool = true
+        
+        func enableStartButton(_ enable: Bool){
+            enableStartButtonWasCalled = true
+            enableStartButtonArgument = enable
+        }
+        func enableStopButton(_ enable: Bool){
+            enableStopButtonWasCalled = true
+            enableStopButtonArgument = enable
+        }
+    }
     
     var mockSensorTag:MockSensorTag!
+    var mockView:View!
     var presenter:Presenter!
     
     override func setUp() {
         super.setUp()
-        mockSensorTag = MockSensorTag()
         presenter = Presenter()
+
+        mockSensorTag = MockSensorTag()
         presenter.sensorTag = mockSensorTag
+        
+        mockView = View()
     }
     
     override func tearDown() {
@@ -48,5 +70,13 @@ class PresenterTests: XCTestCase {
     func testStop() {
         presenter.onStopButtonPressed()
         XCTAssertTrue(mockSensorTag.stopWasCalled)
+    }
+    
+    func testOnCreate() {
+        let returned = presenter.onCreate(mockView)
+        XCTAssertTrue(returned === presenter)
+        XCTAssertTrue(mockView === presenter.ui)
+        XCTAssertTrue(mockView.enableStopButtonWasCalled)
+        XCTAssertFalse(mockView.enableStopButtonArgument)
     }
 }
